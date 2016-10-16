f668b7d9ef1168645d5de311e93d9111f19a269c
Nakamae_ Kazuki <kazuki_nakamae@hotmail.com>
2016-10-16 19:21:19 +0900

    久々に進める

--- python/studyDeeplearning/Study2.py
+++ python/studyDeeplearning/Study2.py
@@ -88,12 +88,13 @@ def predict(network, x):
 x, t = get_data()
 network = init_network()
 
+batch_size=100 #バッチの数
 accuracy_cnt = 0
-for i in range(len(x)):
+for i in range(0, len(x), batch_size):
     #予測
-    y = predict(network,x[i])
-    p = np.argmax(y) #確率が最大のものを取得
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network,x_batch)
+    p = np.argmax(y_batch, axis=1) #確率が最大のものを取得
     #判定
-    if p == t[i]:
-        accuracy_cnt += 1
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
 print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
